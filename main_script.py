32c334600f1066ffc2cc0b43479210dff6d16b6b
Lukas Shum-Tim <36132466+LukasShum-Tim@users.noreply.github.com>
2025-10-13 21:55:30 -0400

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -23,7 +23,7 @@ st.set_page_config(
 )
 
 st.title("🧠 Multilingual Oral Board Exam Trainer")
-st.markdown("Upload a PDF, generate short-answer questions, answer in your chosen language, and get bilingual feedback.")
+st.markdown("Upload a PDF, generate short-answer questions, answer in your chosen language, and get bilingual feedback. If you are using a mobile device, make sure to use a pdf file that is downloaded locally, and not uploaded from a Cloud Drive")
 
 # -------------------------------
 # SESSION STATE INITIALIZATION
